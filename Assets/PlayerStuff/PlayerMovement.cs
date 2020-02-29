36264388bd63990209b0c190224f5a1314927a14
egoulard-2000 <58094995+egoulard-2000@users.noreply.github.com>
2020-02-28 20:43:44 -0700

    Movement script and map

---
+++ Assets/PlayerStuff/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody myRigidbody;
+    public Camera cam;
+
+    private Vector3 movement;
+    private Vector3 movementVelocity;
+
+    void start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player Input
+        movementVelocity = movement * moveSpeed;
+
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, 0f, Input.GetAxisRaw("Vertical") * moveSpeed);
+    }
+}
